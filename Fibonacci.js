8afcedd11506bc425230072213838b4dc50258fc
unknown <befkadu@etmsoftwareplc.com>
2024-10-05 11:37:51 +0300

    Fibonnacci Alogrithm

---
+++ Fibonacci.js
@@ -0,0 +1,14 @@
+function fibonacci(n) {
+    if (n == 0 || n == 1)
+        return "No fibonacci number";
+    const fib = [0, 1]
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
+
+console.log(fibonacci(1)) // "No fibonacci number"
+console.log(fibonacci(2)) //[0,1]
+console.log(fibonacci(3)) //[0,1,1]
+console.log(fibonacci(7)) // [0,1,1,2,3,5,8]
